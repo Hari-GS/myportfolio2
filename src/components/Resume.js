a04b07b79d70abca2d0d0dd2a5f9264931f49c21
Hari G <sakthihariharan007@gmail.com>
2024-11-12 16:22:13 +0530

    resume btn fixed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,7 +14,7 @@ function Resume() {
             <div className='md:w-1/2 flex justify-center'>
                 <div className='flex flex-col justify-center text-white'>
                     <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume</h1>
-                    <p className='pb-5'>You can view my resume <a className='btn' target='_blank' href={config.link}>Click here</a></p>
+                    <p className='pb-5'>You can view my resume <a className='btn' target='_blank' href={config.link}>Here</a></p>
                 </div>
             </div>
         </section>
